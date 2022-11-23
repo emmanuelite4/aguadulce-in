92902842ec57b50901ce925aa41a9b2bf0cf48bc
Emmanuel Alegbeleye <emmanuelite4@gmail.com>
2022-11-23 12:02:05 +0100

    Updated test configuration

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,14 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../testutils";
 import Header from "./Header";
-import { BrowserRouter } from "react-router-dom";
 
 describe("Header / ", function () {
   it("it contains the correct title", () => {
-    render(
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-    );
+    render(<Header />);
     expect(screen.getByText("Role Manager")).toBeInTheDocument();
   });
 });
